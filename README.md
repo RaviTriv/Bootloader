e9fc3f8086aad71e0ff3bc19abfd7e0e3b3d79d7
Ravi <trivediravi561@gmail.com>
2024-12-28 13:31:58 -0500

    print msg on load

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Bootloader
+
+Bios will begin copying from device at address 0x7C00
---
+++ bootloader.asm
@@ -0,0 +1,34 @@
+ORG 0x7C00
+BITS 16
+
+mov  ah,  09h 
+mov  al,  "C" 
+mov  bx,  100b
+mov  cx,  01h 
+int 0x10
+mov  ah, 0Eh
+int  10h
+
+mov ah, 0x0E
+mov al, " "
+int 0x10
+
+mov  ah,  09h 
+mov  al,  "O" 
+mov  bx,  100b
+mov  cx,  01h 
+int 0x10
+mov  ah, 0Eh
+int  10h
+
+mov  ah,  09h 
+mov  al,  "S" 
+mov  bx,  100b
+mov  cx,  01h 
+int 0x10
+mov  ah, 0Eh
+int  10h
+
+jmp $
+times 510 - ($-$$) db 0
+dw 0xAA55
